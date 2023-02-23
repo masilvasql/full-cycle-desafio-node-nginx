0a6291ed55dc535131d6bc128a1e98e978dc7cb3
marcelo.silva <marcelo.silva@neomind.com.br>
2023-02-22 23:28:17 -0300

    desafio nodejs-nginx

---
+++ nodeJS/index.js
@@ -0,0 +1,48 @@
+import axios from "axios"
+import express from "express"
+const app = express()
+
+const config = {
+    host: "db",
+    user: "root",
+    password: "root",
+    database: "nodedb",
+}
+
+import mysql from "mysql"
+
+const connection = mysql.createConnection(config)
+
+app.use(express.json())
+const sql = `INSERT INTO people(name) values('Marcelo')`
+connection.query(sql)
+
+
+app.get("/", async (req, res) => {
+    let resultado = await findAllPeople()
+
+    let html = "<h1>Hello Full Cycle</h1>"
+    html += "<table>"
+    resultado.forEach(element => {
+        html += `<tr><td>${element.ID} - ${element.name}</td></tr>`
+    });
+    html += "</table>"
+    return res.status(200).send(html)
+})
+
+
+function findAllPeople() {
+    return new Promise(resolve => {
+        const sql = "SELECT * FROM people"
+        connection.query(sql, (err, results, fields) => {
+            if (err) {
+                throw err
+            }
+            resolve(results)
+        })
+    })
+}
+
+app.listen(3000, () => {
+    console.log("Rodando!!!")
+})
